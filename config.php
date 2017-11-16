8a997b77ce5f5990e50622580963578f963af670
Fabiano <fabiano_hatori@yahoo.com.br>
2017-11-16 17:01:02 -0200

    Check security

--- config.php
+++ config.php
@@ -7,22 +7,12 @@
 	//header("location:manutencao.php");
 	global $conn, $caminhoSistema, $tituloSistema, $descricaoSistema, $tipoBaseDados, $dadosUserLogin, $modulosAtivos, $modulosGeral;
 
-	// $caminhoSistema 		= "http://sistema.eficazsystem.com.br";
-	// $caminhoFisico 		= "/home/storage/6/ca/a4/eficazsystem3/public_html/sistema";
-	// $tituloSistema 		= "Eficaz System - Helpdesk";
-	// $descricaoSistema 	= "";
-	// $dataExpira		 	= "01/01/2030";
 
 	$caminhoSistema 	= "http://eficazsystem.com.localsistema";
 	$caminhoFisico 		= "C:/wamp64/www/sistemaEficaz";
 	$tituloSistema 		= "Eficaz System - LocalHelpdesk";
 	$descricaoSistema 	= "Backup do sistema de atendimento da Eficaz System";
 	$dataExpira		 	= "01/01/2030";
-
-
-	// $conn = mysql_connect("mysql01.eficazsystem3.hospedagemdesites.ws","eficazsystem3","fpinfo2981")or die("Nao foi possivel conectar o BD.");
-	// mysql_select_db("eficazsystem3") or die("Nao foi possivel selecionar o BD.");
-
 
 	$conn = mysql_connect("localhost","root","")or die("Nao foi possivel conectar o BD.");
 	mysql_select_db("eficazsystem3") or die("Nao foi possivel selecionar o BD.");
